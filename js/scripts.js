001ee55b8110db43dfac965501d5f7182f2c6fc9
Adrian Agnic and Roger Tobias <adrian+roger@>
2017-05-04 15:47:54 -0700

    adds the working logic to make the string in its cryptoform.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
+//-----------------------------------------FRONT END
 $(document).ready(function() {
   $("#submitForm").submit(function(event) {
     event.preventDefault();
-    debugger;
-    var userInput = $("input#userInput").val();
 
-    $("#output").text(result);
+    var userInput = $("input#userInput").val().replace(/\s/g,'');
+    numRows(userInput);
+
+
+    // $("#output").text(!!!!!);
   });
 });
+
+//-------------------------------------------BACK END
+var numRows = function(userInput) {
+  debugger;
+  var stringLength = userInput.length;
+
+  var rows = Math.ceil(Math.sqrt(stringLength));
+
+  var newSent = "";
+  var x = rows;
+
+  for (j = 0; j < x; j++) {
+    debugger;
+    for (i = 0; i < stringLength; i+=x) {
+      newSent += userInput.charAt(i + j);
+    }
+  }
+  return newSent;
+
+  console.log(rows);
+};
